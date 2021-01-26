b49048c81e0c3fbe778d14ab8fa88e3eb1fc6690
adlt-fiu <anthony3dlt@gmail.com>
2021-01-26 12:29:24 -0500

    Initial Commit

---
+++ MainWW/MainWWApp.swift
@@ -0,0 +1,17 @@
+//
+//  MainWWApp.swift
+//  MainWW
+//
+//  Created by Anthony de la Torre on 1/26/21.
+//
+
+import SwiftUI
+
+@main
+struct MainWWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
